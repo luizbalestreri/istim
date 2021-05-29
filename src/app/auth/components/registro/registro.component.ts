1bfcd3954ec5170e6d3f09710ccc556c98a6fe2b
LuizBalestreri <balesss@gmail.com>
2021-05-28 22:05:46 -0300

    Registro alterado

--- src/app/auth/components/registro/registro.component.ts
+++ src/app/auth/components/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators  } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService, ILoginCredentials } from 'src/app/core/auth/auth.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _authService: AuthService,
+  private _router: Router,
+  private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
-
+  form: FormGroup = this._formBuilder.group({
+    email: [
+      '',
+      [Validators.required, Validators.email],
+    ],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+  
+  submit(): void {
+    let data: ILoginCredentials = this.form.getRawValue();
+    this._authService.authenticate(data).subscribe(
+      (res) => {
+        console.log('Sucesso');
+        this._router.navigate(['/']);
+      },
+      (err) => {
+        console.log('Erro');
+      }
+    );
+  }
 }
