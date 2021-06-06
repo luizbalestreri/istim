bc9f298d60499c2a0909be31dab653338ba88493
LuizBalestreri <balesss@gmail.com>
2021-06-06 19:49:04 -0300

    core atualizado para paginator

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,20 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RequestInterceptor } from './interceptors/request-interceptor';
+import { GameDataService } from './services/game-data.service';
+import { PaginationService } from './services/pagination.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
+  imports: [HttpClientModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: RequestInterceptor,
       multi: true,
     },
+    GameDataService,
+    PaginationService
   ],
 })
 export class CoreModule {}
